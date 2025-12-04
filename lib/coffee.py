db08ff59861b8ea0192dc5641064244ad92ad537
Joana Corezzi Williams <joanacorezzi@gmail.com>
2025-12-04 11:38:01 -0600

    Add Book and Coffee classes, update README, all tests passing

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+
+        # create a "hidden" size variable
+        self._size = None
+
+        # use the property so the validation runs
+        self.size = size
+
+        # store the price (we assume it's a number)
+        self.price = price
+
+    # getter for size
+    @property
+    def size(self):
+        return self._size
+
+    # setter for size
+    @size.setter
+    def size(self, value):
+        # only accept Small, Medium, or Large
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    # tipping
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        # increase price by 1
+        self.price += 1
